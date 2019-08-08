bd9fd14c35225b1caed9b171acff8ee565107817
ykamiki <kamiki.internous@gmail.com>
2019-08-08 17:35:22 +0900

    myecsite 2019/08/08

---
+++ myecsite/src/main/java/com/internous/myecsite/MyecsiteApplication.java
@@ -0,0 +1,13 @@
+package com.internous.myecsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyecsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyecsiteApplication.class, args);
+	}
+
+}
